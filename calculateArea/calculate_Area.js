519755881157feae3133553d3e1ca0c3d69d8bec
Jamie Carter <jamie.carter@lmco.com>
2024-07-07 11:24:58 -0400

    JS Functions Lesson

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,17 @@ function calculateArea() {
     console.log(area);
     document.getElementById('result').innerText = `The Area of the rectangle is: ${area}`;
 }
+
+function calculateTotalAmount() {
+    let grocery1 = parseFloat(document.getElementById('grocery1').value);
+    let grocery2 = parseFloat(document.getElementById('grocery2').value);
+    let grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    doCalculation(grocery1, grocery2, grocery3);
+}
+
+function doCalculation(grocery1, grocery2, grocery3) {
+    let total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('result').innerText = `The total amount is: \$${total}`;
+}
